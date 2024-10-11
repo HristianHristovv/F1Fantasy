23aa836327aebda64f8773a96b35f0ed4cd45ddf
pc1_6 <pc1_6@DESKTOP-VQ4OQHT>
2024-10-11 12:00:15 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace F1Fantasy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
